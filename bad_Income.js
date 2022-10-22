33aa5d5b687621296c391505c2cc7c76addb4afd
ngokchuenli <98185774+ngokchuenli@users.noreply.github.com>
2022-10-21 22:50:00 -0500

    Add files via upload

---
+++ bad_Income.js
@@ -0,0 +1,65 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+};
+
+var calculateIncome = function () 
+{
+    var floatAnnualIncome = parseFloat($("annualIncome").value);
+    var intUserAge = parseInt($("Age").value);
+    var floatTaxRate;
+    
+    if (intUserAge >= 65)
+    {
+    	if (floatAnnualIncome <= 30000)
+    	{
+    		floatTaxRate = 2.5;
+    	}
+    	else
+    	{
+    		if(floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
+    		{
+    			floatTaxRate = 5;
+    		}
+    		else
+    		{
+    			floatTaxRate = 20;
+    	    }
+        }
+    }
+    else
+    {
+
+    	if (floatAnnualIncome > 30000)
+    	{
+    		floatTaxRate =5;
+    	}
+   		if (floatAnnualIncome => 30000 && floatAnnualIncome <= 75000)
+   		{
+   			floatTaxRate =10;
+   		}
+   		else
+   		{
+   			floatTaxRate =20;
+   		}
+	}
+};
+
+	floatAnnualTax = floatAnnualIncome * (floatTaxRate/100);
+
+	$("annualTax").value = floatAnnualTax;
+
+
+	alert ("Your annual income tax this year is $" + floatAnnualTax  +"\nBased upon an annual income of $"+ floatAnnualIncome+
+	"\nAnd a tax rate of " +floatTaxRate+ "% \nNote: Tax rate is based upon your age");
+	
+
+
+window.onload = function () 
+{
+    $("AnnualIncome").value = "";
+    $("Age").value = "";
+    $("annualTax").value = "";
+    $("calculate").onclick = calculateIncome;
+    $("annualIncome").focus();
+};
