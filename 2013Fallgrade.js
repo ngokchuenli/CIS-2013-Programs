294f486e1e0bdfb0f91dfbd13515919b88374e2b
ngokchuenli <98185774+ngokchuenli@users.noreply.github.com>
2022-10-24 02:18:28 -0500

    Add files via upload

---
+++ 2013Fallgrade.js
@@ -0,0 +1,51 @@
+//Declare variables intGradeOption, floatTotalPts, floatHwPts, floatMidPts, floatFinPts, stringFinalGrade
+//Get user input to determine grade
+//Calculate floatTotalPts (sum of HW and exam pts)
+//Get user input on whether the course is audit pass/fail (1) or letter grade (2)
+//If the grade option is audit, evaluate total score based upon 80% cutoff scorefor pass/fail
+//Else determine letter grade based upon the typical A = 90%, B - 80%, etc.
+//Output stringFinalGrade
+
+var intGradeOption, floatTotalPts, floatHwPts, floatMidPts, floatFinPts, stringFinalGrade;
+
+floatHwPts = parseFloat(prompt("Please enter final Homework points (0-30):"));
+
+floatMidPts = parseFloat(prompt("please enter midterm points(0-35):"));
+
+floatFinPts = parseFloat(prompt("please enter final points(0-35):"));
+
+floatTotalPts = (floatHwPts+floatMidPts+floatFinPts);
+
+intGradeOption = parseFloat(prompt("please enter ingradeoption(1 && 2)"));
+
+if (intGradeOption===1)
+{
+    if(floatTotalPts >= 90 && floatTotalPts<100);
+       stringFinalGrade="A";
+}
+else if (floatTotalPts>=80 && floatTotalPts<90);
+{
+    stringFinalGrade="B"
+}
+ if (floatTotalPts>=70 && floatTotalPts<80);
+{
+    stringfinalgrade="C"
+}
+ if (floatTotalPts>=60 && floatTotalPts<70);
+{
+    stringfinalgrade="D"
+}
+ if (intGradeOption==="2")
+{
+    if (floatTotalPts>=80);
+    {
+        stringfinalgrade="pass"
+    }
+ if (floatTotalPts,80)
+    {
+         stringfinalgrade="fail"
+    }
+
+
+alert("Your final grade is: " +stringFinalGrade)
+};
