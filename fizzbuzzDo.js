79e77190466c61ec6ff5a08a50098895b284e4e0
ngokchuenli <98185774+ngokchuenli@users.noreply.github.com>
2022-10-28 22:59:05 -0500

    Add files via upload

---
+++ fizzbuzzDo.js
@@ -0,0 +1,54 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+};
+
+doStuffComputer = function()
+{
+//	whileFunction();
+	//forFunction();
+	doWhileFunction();
+};
+
+
+function doWhileFunction()
+{
+	var counter = 1;
+	var output = "";	
+
+    do
+    {
+        if(counter% 15===0)
+        {
+            output=output+"FizzBuzz\n";
+            counter++;
+        }
+        else if(counter %5===0)
+        {
+            output=output + "Buzz\n";
+            counter++;
+        }
+        else if(counter %3===0)
+        {
+            output=output + "Fizz\n";
+            counter++;
+        }
+        else 
+        {
+            output=output + counter +"\n";
+            counter++;
+        }
+    }while(counter<=100)
+    
+    alert(output);
+    };
+
+
+
+
+
+
+window.onload = function()
+{
+	$("doIt").onclick = doStuffComputer;
+};
