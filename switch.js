7458aa65d464a887345aaff0a6284c74e85fc6a2
ngokchuenli <98185774+ngokchuenli@users.noreply.github.com>
2022-11-08 00:14:42 -0600

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,61 @@
+var $ = function (id) {
+    return document.getElementById(id);
+};
+
+var fixPrinter = function() {
+    var stringOutput = "";  //declares stringOutput inital value to be blank
+
+    //define the individual troubleshooting tips per the chart
+    var stringMessage1 = "Check the power cable.";
+    var stringMessage2 = "Check the printer-computer cable.";
+    var stringMessage3 = "Ensure printer software is installed.";
+    var stringMessage4 = "Check / replace ink.";
+    var stringMessage5 = "Check for paper jam.";
+    var stringMessage6 = "Looks like everything is working fine!";
+
+    /* Need to be careful here when defining our boolean variables for use in the troubleshooting chart
+     * and what you define as true or false.  For example, in the chart used in this problem,
+     * a combination of Y-Y-Y results in messages 2, 3, & 4 being provided
+     * to the user.  In this case, Y-Y-Y means
+     * "the printer does NOT print"  AND "a red light is flashing" AND "the printer is unrecognized"
+     */
+    var boolPrinting = $("not_printing").selected;  //returns a value of true only when the not_printing html id is selected from the dropdown menu
+    var boolRedLight = $("yes_redlight").selected;  //returns a value of true only when the yes_redlight html id is selected from the dropdown menu
+    var boolRecognised = $("not_recognised").selected;  ////returns a value of true only when the not_recognized html id is selected from the dropdown menu
+
+    //enter your code here and don't forget to send the output to the DOM
+    switch (true) {
+    case boolPrinting === true && boolRedLight === true && boolRecognised === true:
+        stringOutput = stringMessage2 + "\n" + stringMessage3 + "\n" + stringMessage4;
+        break;
+    case boolPrinting === true && boolRedLight === true && boolRecognised === false:
+        stringOutput = stringMessage4 + "\n" + stringMessage5;
+        break;
+    case boolPrinting === true && boolRedLight === false && boolRecognised === true:
+        stringOutput = stringMessage1 + "\n" + stringMessage2 + "\n" + stringMessage3;
+        break;
+    case boolPrinting === true && boolRedLight === false && boolRecognised === false:
+        stringOutput = stringMessage5;
+        break;
+    case boolPrinting === false && boolRedLight === true && boolRecognised === true:
+        stringOutput = stringMessage3 + "\n" + stringMessage4;
+        break;
+    case boolPrinting === false && boolRedLight === true && boolRecognised === false:
+        stringOutput = stringMessage4;
+        break;
+    case boolPrinting === false && boolRedLight === false && boolRecognised === true:
+        stringOutput = stringMessage3;
+        break;
+    case boolPrinting === false && boolRedLight === false && boolRecognised === false:
+        stringOutput = stringMessage6;
+        break;
+    default:
+        stringOutput = " ";
+        break;
+    }
+    $("output").value = stringOutput;
+    
+};
+ window.onload = function () {
+    $("troubleshoot").onclick = fixPrinter;
+};
