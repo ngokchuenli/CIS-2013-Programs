42e0b20acbfc16bda62ad3707f55ae7c9bd7fdbe
ngokchuenli <98185774+ngokchuenli@users.noreply.github.com>
2022-11-30 13:13:44 -0600

    Add files via upload

--- ArrayPracticeEC.js
+++ ArrayPracticeEC.js
@@ -29,9 +29,31 @@ for (var x=1; x<=12; x++)
 alert(stringTimesTable);
 
 
-
-
-
-
-
 //task 3
+var stringEvenTimes = "";
+var timesTable =[];
+
+for(var a=1; a<=12; a++)
+{
+    timesTable[a] = [];
+    for(var b = 1; b <= 12; b++)
+    {
+        timesTable[a][b] = a*b;
+    }
+    
+}
+alert(timesTable);
+
+for (var i=1; i<=12; i++)
+{
+    if(i%2===0)
+    {
+        for (var j=1; j<=12; j++)
+        {
+            stringEvenTimes = stringEvenTimes + " " + timesTable[i][j] + " ";
+        }
+        stringEvenTimes += "\n";
+    }
+}
+
+alert (stringEvenTimes);
